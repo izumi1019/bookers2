59df817c7414f5573f747755a7557295c10a116e
izumi1019 <khm1_frog@outlook.jp>
2021-02-10 05:02:06 +0000

    devise関連まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+ root "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  resources :users
+  resources :books
+  resources :homes
 end
+
